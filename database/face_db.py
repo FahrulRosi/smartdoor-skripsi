58b7a7d6ef122fc83f8967ee2d1d2bceae0e55c0
oci46 <fahrulr358@gmail.com>
2026-04-19 16:24:21 +0700

    get_all_faces

--- database/face_db.py
+++ database/face_db.py
@@ -13,3 +13,19 @@ class FaceDB:
 
     def save(self, data):
         pickle.dump(data, open(self.path, "wb"))
+
+# --- Tambahkan helper functions di bawah ini ---
+_db = FaceDB()
+
+def get_all_faces():
+    """Mengambil semua data wajah yang terdaftar dari file pkl"""
+    return _db.load()
+
+def save_face(name, embedding):
+    """Menyimpan embedding wajah baru ke dalam file pkl"""
+    faces = _db.load()
+    faces.append({
+        "name": name,
+        "embedding": embedding
+    })
+    _db.save(faces)

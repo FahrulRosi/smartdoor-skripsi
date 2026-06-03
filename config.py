e51b9aaff884a949b3bad614975f045973c57640
oci46 <fahrulr358@gmail.com>
2026-06-03 16:07:16 +0700

    pembaruan

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ CLAHE_TILE_GRID_SIZE     = (8, 8)
 
 # ── Database Supabase ────────────────────────────────────────────────────────
 SUPABASE_URL = "https://gwwxebdmavlmxxcdrlge.supabase.co"
-SUPABASE_KEY = "sb_publishable_tUsmI6J1BubOlRf9YhtNPQ_F9dWIyOv"
+
 
 # ── FaceMesh ─────────────────────────────────────────────────────────────────
 MIN_DETECTION_CONFIDENCE = 0.5
@@ -58,7 +58,7 @@ MOBILEFACENET_PATH      = "recognition/mobilefacenet.onnx"
 
 # ── Door Lock & UI ───────────────────────────────────────────────────────────
 LOCK_GPIO_PIN   = 18
-BUTTON_PIN      = 20
+BUTTON_PIN      = 23
 UNLOCK_DURATION = 5   
 
 # Definisi Warna BGR untuk OpenCV

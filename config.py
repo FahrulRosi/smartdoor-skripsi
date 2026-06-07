a4f1550ec5b9488ba91d541e6e708d7c54e5a3c4
oci46 <fahrulr358@gmail.com>
2026-06-07 14:32:22 +0700

    app.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # ── Camera & Hardware ────────────────────────────────────────────────────────
 CAMERA_INDEX    = 0
-FRAME_WIDTH     = 1280
-FRAME_HEIGHT    = 720
+FRAME_WIDTH     = 640
+FRAME_HEIGHT    = 480
 FPS             = 30
 
 # ── Peningkatan Gambar (Low-Light & Backlight) ───────────────────────────────
